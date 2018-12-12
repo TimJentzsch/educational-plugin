fdb20ae70e98939c9a66b6f8fec55699be174950
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-12-12 16:58:12 +0300

    Update `CourseFormatTest`

--- educational-core/testSrc/com/jetbrains/edu/learning/CourseFormatTest.java
+++ educational-core/testSrc/com/jetbrains/edu/learning/CourseFormatTest.java
@@ -10,21 +10,13 @@ import com.jetbrains.edu.learning.courseFormat.tasks.Task;
 import com.jetbrains.edu.learning.serialization.SerializationUtils;
 import com.jetbrains.edu.learning.stepik.StepikNames;
 import org.jetbrains.annotations.NotNull;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TestName;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
-
 
 public class CourseFormatTest extends EduTestCase {
-  @Rule
-  public TestName name = new TestName();
 
-  @Test
   public void testAdditionalMaterialsLesson() throws IOException {
     final Course course = getCourseFromJson();
     final List<Lesson> lessons = course.getLessons(true);
@@ -36,7 +28,6 @@ public class CourseFormatTest extends EduTestCase {
     assertNull(oldAdditional);
   }
 
-  @Test
   public void testCourseWithSection() throws IOException {
     final Course course = getCourseFromJson();
     final List<StudyItem> items = course.getItems();
@@ -46,7 +37,6 @@ public class CourseFormatTest extends EduTestCase {
     assertEquals(1, ((Section)items.get(0)).getLessons().size());
   }
 
-  @Test
   public void testFrameworkLesson() throws IOException {
     final Course course = getCourseFromJson();
     final List<StudyItem> items = course.getItems();
@@ -54,7 +44,6 @@ public class CourseFormatTest extends EduTestCase {
     assertTrue(items.get(0) instanceof FrameworkLesson);
   }
 
-  @Test
   public void testPycharmToEduTask() throws IOException {
     final Course course = getCourseFromJson();
     final List<Lesson> lessons = course.getLessons();
@@ -65,13 +54,11 @@ public class CourseFormatTest extends EduTestCase {
     assertTrue(taskList.get(0) instanceof EduTask);
   }
 
-  @Test
   public void testDescription() throws IOException {
     EduTask eduTask = getFirstEduTask();
     assertEquals("First task description", eduTask.getTaskDescription(false, null));
   }
 
-  @Test
   public void testFeedbackLinks() throws IOException {
     EduTask eduTask = getFirstEduTask();
 
@@ -93,7 +80,6 @@ public class CourseFormatTest extends EduTestCase {
     return (EduTask)task;
   }
 
-  @Test
   public void testHint() throws IOException {
     EduTask eduTask = getFirstEduTask();
     final TaskFile taskFile = eduTask.getTaskFile("task.py");
@@ -105,7 +91,6 @@ public class CourseFormatTest extends EduTestCase {
     assertEquals("my first hint", hints.get(0));
   }
 
-  @Test
   public void testPlaceholderText() throws IOException {
     EduTask eduTask = getFirstEduTask();
     final TaskFile taskFile = eduTask.getTaskFile("task.py");
@@ -115,7 +100,6 @@ public class CourseFormatTest extends EduTestCase {
     assertEquals("write function body", answerPlaceholders.get(0).getPlaceholderText());
   }
 
-  @Test
   public void testPossibleAnswer() throws IOException {
     EduTask eduTask = getFirstEduTask();
     final TaskFile taskFile = eduTask.getTaskFile("task.py");
@@ -125,57 +109,26 @@ public class CourseFormatTest extends EduTestCase {
     assertEquals("pass", answerPlaceholders.get(0).getPossibleAnswer());
   }
 
-  @Test
   public void testCourseName() throws IOException {
     final Course course = getCourseFromJson();
     assertEquals("My Python Course", course.getName());
   }
 
-  @Test
   public void testCourseProgrammingLanguage() throws IOException {
     final Course course = getCourseFromJson();
     assertEquals(EduNames.PYTHON, course.getLanguageID());
   }
 
-  @Test
   public void testCourseLanguage() throws IOException {
     final Course course = getCourseFromJson();
     assertEquals("Russian", course.getHumanLanguage());
   }
 
-  @Test
   public void testCourseDescription() throws IOException {
     final Course course = getCourseFromJson();
     assertEquals("Best course ever", course.getDescription());
   }
 
-  @Test
-  public void testTestFiles() throws IOException {
-    final Course course = getCourseFromJson();
-    final List<Lesson> lessons = course.getLessons();
-    assertFalse("No lessons found", lessons.isEmpty());
-    final Lesson lesson = lessons.get(0);
-    final List<Task> taskList = lesson.getTaskList();
-    assertFalse("No tasks found", taskList.isEmpty());
-    final Task task = taskList.get(0);
-    assertEquals(1, task.getTestsText().size());
-  }
-
-  @Test
-  public void testTestFilesCustomName() throws IOException {
-    final Course course = getCourseFromJson();
-    final List<Lesson> lessons = course.getLessons();
-    assertFalse("No lessons found", lessons.isEmpty());
-    final Lesson lesson = lessons.get(0);
-    final List<Task> taskList = lesson.getTaskList();
-    assertFalse("No tasks found", taskList.isEmpty());
-    final Task task = taskList.get(0);
-    final Map<String, String> testsText = task.getTestsText();
-    assertEquals(2, testsText.size());
-    assertTrue(testsText.containsKey("super_test.py"));
-  }
-
-  @Test
   public void testStudentTaskText() throws IOException {
     final Course course = getCourseFromJson();
     final List<Lesson> lessons = course.getLessons();
